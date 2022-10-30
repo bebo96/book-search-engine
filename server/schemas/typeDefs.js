6da1d39811003f29ea9ccd9d245b63e3804b938a
bebo <ibrahimimran96@gmail.com>
2022-10-30 19:58:26 -0400

    Added typeDefs and mutation for login, addUser, saveBook, removeBook

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -34,7 +34,7 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(savedBook: bookInput!): User
+    saveBook(bookToSave: bookInput): User!
     removeBook(bookId: ID!): User
   }
 `;
