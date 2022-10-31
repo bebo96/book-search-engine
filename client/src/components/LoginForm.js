5653db6bff482fdd48737fb121187afe4d47222e
bebo <ibrahimimran96@gmail.com>
2022-10-30 20:01:53 -0400

    Modified LoginForm to use GraphQL routing

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,14 +1,15 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { loginUser } from '../utils/API';
+import {useMutation} from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+  const [loginUser, {error}] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -26,16 +27,14 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
+      const {data} = await loginUser(
+        {
+          variables: {...userFormData}
+        }
+      );
+      
+      Auth.login(data.loginUser.token);
+    } catch(err){
       console.error(err);
       setShowAlert(true);
     }
@@ -51,7 +50,7 @@ const LoginForm = () => {
     <>
       <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
         <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
-          Something went wrong with your login credentials!
+          {error && "Something went wrong with your login credentials!"}
         </Alert>
         <Form.Group>
           <Form.Label htmlFor='email'>Email</Form.Label>
