90e5475134e64ef5e4f5c546528dcf6ad69e2030
bebo <ibrahimimran96@gmail.com>
2022-10-30 20:41:12 -0400

    Deploying

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Book Search Engine Starter Code
+
+## Purpose
+A book search engine which allows users to save books and view them in a "Saved Books" page
+
+## Built With 
+* React JS
+* Apollo
+* GraphQL
+* Mongoose
+* Bcrypt
+* Bootstrap
+
+## Repository
+https://github.com/bebo96/book-search-engine
+
+## Deployed Application 
+https://bebo96.github.io/react-portfolio/
+
+## Contribution
+Made with ❤️ by BEBO
+
+### ©️2022 React Portfolio
--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
-
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/module-21-challenge',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);
 module.exports = mongoose.connection;
